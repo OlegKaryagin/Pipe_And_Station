3e02b04e94c1eb6d768084735c6d72b5c589e27d
OlegKaryagin <63190353+OlegKaryagin@users.noreply.github.com>
2020-10-13 21:45:55 +0300

    Two struct and two function
    Created two function for  pipe parametrs.

--- Pipe/Pipe/Pipe.cpp
+++ Pipe/Pipe/Pipe.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 
 
-struct pipe
+struct Pipe
 {
 	short id;
 	int length;
@@ -15,7 +15,7 @@ struct pipe
 
 
 
-struct station
+struct Station
 {
 	short id;
 	string name;
@@ -26,7 +26,36 @@ struct station
 
 
 
+ Pipe Create_New_Pipe()
+{
+	Pipe New_Pipe;
+	cout << "Please, enter the pipe id" << endl;
+	cin >> New_Pipe.id;
+	cout << "Please, enter the pipe length (mm)" << endl;
+	cin >> New_Pipe.length;
+	cout << "Please, enter the pipe diametr (mm)" << endl;
+	cin >> New_Pipe.diametr;
+	cout << "Please, enter the pipe status (work/repair)" << endl;
+	cin >> New_Pipe.status;
+	return New_Pipe;
+
+}
+
+
+
+void Print_Pipe(Pipe New_Pipe)
+{
+	cout << "Pipe ID:\t" << New_Pipe.id
+		<< "\tPipe length:\t" << New_Pipe.length
+		<< "\tPipe diametr:\t" << New_Pipe.diametr
+		<< "\tPipe status:\t" << New_Pipe.status << endl;
+
+}
+
+
 int main()
 {
-
+	Pipe Preset_Pipe = Create_New_Pipe();
+	Print_Pipe(Preset_Pipe);
+	return 0;
 }
