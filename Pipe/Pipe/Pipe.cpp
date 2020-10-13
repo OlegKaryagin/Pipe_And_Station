50782aa38fcf3a1f88a36d4fd3e9be8553e34108
OlegKaryagin <63190353+OlegKaryagin@users.noreply.github.com>
2020-10-13 21:02:33 +0300

    Two struct
    Version with two struct

---
+++ Pipe/Pipe/Pipe.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+
+
+struct pipe
+{
+	short id;
+	int length;
+	int diametr;
+	string status;
+};
+
+
+
+struct station
+{
+	short id;
+	string name;
+	int number_of_workshops;
+	int workshops_in_operation;
+	int efficiency;
+};
+
+
+
+int main()
+{
+
+}
